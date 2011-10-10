0c391208dd147e5ade2b3acf74e040a1665df964
vamitrou@gmail.com <vamitrou@gmail.com@9f93cd60-5c5c-d33d-18db-9371d0e3ccf2>
2011-10-10 17:22:39 +0000

    -scroll implemented -zoom implemented -WARNING! DIRTY CODE DETECTED

--- trunk/ava-project/src/com/epl603/ava/classes/PointPath.java
+++ trunk/ava-project/src/com/epl603/ava/classes/PointPath.java
@@ -4,36 +4,61 @@ import java.util.ArrayList;
 
 import android.graphics.Path;
 import android.graphics.Point;
+import android.graphics.PointF;
 
 public class PointPath extends Path{
-	public ArrayList<Point> points;
-
+	public ArrayList<PointF> points;
+	//public float dx = 0;
+	//public float dy = 0;
 	
 	public PointPath()
 	{
 		super();
-		points = new ArrayList<Point>();
+		points = new ArrayList<PointF>();
 	}
 	
-	public void addPoint(float x, float y)
+	public PointPath(ArrayList<PointF> pts)
 	{
+		super();
+		points = pts;
+		this.moveTo(pts.get(0).x, pts.get(0).y);
+		for (int i=1; i<pts.size(); i++)
+		{
+			this.lineTo(pts.get(i).x, pts.get(i).y);
+		}
+	}
+	
+	//public void addPoint(float x, float y)
+	//{
+	//	addPoint(x, y, 0, 0, 1);
+	//}
+	
+	public void addPoint(float x, float y, float dx, float dy, float scale)
+	{
+		if (scale == 0)
+			scale = 1;
+		
 		if (points == null)
 		{
-			points = new ArrayList<Point>();
+			points = new ArrayList<PointF>();
 		}
 		
 		if (points.size() == 0)
 		{
-			this.moveTo(x, y);
+			//this.moveTo(x- dx, y- dy);
+			this.moveTo((x-dx)/scale, (y-dy)/scale);
 		}
 	
-		this.lineTo(x, y);
-		points.add(new Point((int) x, (int) x));
+		this.lineTo((x-dx)/scale, (y-dy)/scale);
+		points.add(new PointF((x+dx)*scale, (x+dx)*scale));
+		
+		//this.dx = dx;
+		//this.dy = dy;
 	}
 	
-	public Point getPoint(int index)
+	public PointF getPoint(int index)
 	{
-		return (Point)points.get(index);
+		return (PointF)points.get(index);
 	}
 	
 	public int getPointsCount()
@@ -45,8 +70,32 @@ public class PointPath extends Path{
 	}
 	
 	//aris added this, to export points array list
-	public ArrayList<Point> getROIpoints(){
+	public ArrayList<PointF> getROIpoints(){
 		return points;
 	}
+	
+	/*class TranslatedPoint extends TranslatedPoint
+	{
+		public TranslatedPoint(int x, int y)
+		{
+			super(x, y);
+			dx = 0;
+			dy = 0;
+			scale = 1;
+		}
+		
+		public TranslatedPoint(int x, int y, float dx, float dy, float scale)
+		{
+			super(x, y);
+			this.dx = dx;
+			this.dy = dy;
+			this.scale = scale;
+		}
+		
+	
+		public float scale;
+		public float dx;
+		public float dy;
+	}*/
 
 }
